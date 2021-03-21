a8d254f25525be5413c05d8a4729090852e4fc55
chuzhongzai <qq1822574439@gmail,com>
2021-03-21 22:55:58 +0800

    瞎写的一个登陆界面

--- main.py
+++ main.py
@@ -11,12 +11,38 @@ class Application(Frame):
         self.createWidget()
 
     def createWidget(self):
-       self.l=Label(self, width=10, height=5)
-       self.l["text"]="emmm"
-       self.l.pack()
+        self.label01 = Label(self, text="用户名")
+        self.label01.pack()
 
-    def emmm(self):
-        messagebox.showinfo("emm", "woshiyigechuangkou")
+        v1 = StringVar()
+        v1.set("abc")
+        self.entry01 = Entry(self, textvariable=v1)
+        self.entry01.pack()
+
+        self.label02 = Label(self, text="密码")
+        self.label02.pack()
+
+        v2 = StringVar()
+        v2.set("123")
+        self.entry02 = Entry(self, textvariable=v2)
+        self.entry02.setvar("123")
+        self.entry02.pack()
+
+        self.btn01 = Button(self, text="LOGIN", command=self.login)
+        self.btn01.pack()
+
+    def login(self, ):
+
+        username=self.entry01.get()
+        password=self.entry02.get()
+        print("用户名：" + self.entry01.get())
+        print("密码： " + self.entry02.get())
+        f = open('test.txt', "r")
+        a = f.read().split('\n')
+        if a[0] == username and a[1] == password :
+            messagebox.showinfo(self, message="登陆成功")
+        else:
+            messagebox.showinfo(self, message="登陆失败")
 
 
 root = Tk()
